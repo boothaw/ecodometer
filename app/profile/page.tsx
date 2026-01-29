030ff89a09638e80392aba6992402e23a58bbc10
boothaw <andersonwilkesbooth@gmail.com>
2026-01-28 22:38:09 -0500

    layout and pages

---
+++ app/profile/page.tsx
@@ -0,0 +1,15 @@
+export default function Profile() {
+  return (
+    <div className="flex min-h-screen items-center justify-center font-body">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
+        <div className="flex flex-col items-center justify-center mx-auto gap-6 text-center">
+          <h1 className="max-w-xs text-3xl font-semibold text-navy font-display">
+            Profile<br></br>(vehicles list here)
+          </h1>
+      
+        </div>
+
+      </main>
+    </div>
+  );
+}
--- app/vehicle/page.tsx
+++ app/vehicle/page.tsx
@@ -1,14 +1,12 @@
-import Image from "next/image";
-
-export default function Home() {
+export default function Vehicles() {
   return (
     <div className="flex min-h-screen items-center justify-center font-body">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50 font-display">
-            ECOdometer
+          <h1 className="max-w-xs text-3xl font-semibold text-navy font-display">
+            Vehicle - change to id routing ? 
           </h1>
-      
+      <a className="btn-ghote" href="/new-vehicle">New Vehicle</a>
         </div>
 
       </main>
