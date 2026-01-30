b88fd4efa99efddc81702f4eb2659ccac2fb151e
boothaw <andersonwilkesbooth@gmail.com>
2026-01-30 16:37:17 -0500

    vehicle id page added

--- src/app/profile/[profileId]/vehicle/[vehicleId]/page.tsx
+++ src/app/profile/[profileId]/vehicle/[vehicleId]/page.tsx
@@ -1,17 +1,17 @@
 export default async function Vehicles({
   params,
 }: {
-  params: Promise<{ vehicleId: string }>
+  params: Promise<{ profileId: string; vehicleId: string }>
 }) {
 
-  const {vehicle} = await vehicleId
+  const { profileId, vehicleId } = await params
 
   return (
     <div className="flex min-h-screen items-center justify-center font-body">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+        <div className="mx-auto flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold text-navy font-display">
-            Vehicle - ID: {vehicle}
+            Vehicle - ID: {vehicleId} <br></br> (Profile: {profileId})
           </h1>
       <a className="btn" href="/new-vehicle">New Vehicle</a>
         </div>
