b820dead66f763919ed7061fb7f70ff5dabe5af3
boothaw <andersonwilkesbooth@gmail.com>
2026-01-30 14:23:32 -0500

    styles updates

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,12 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center font-body">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50 font-display">
+        <div className="flex flex-col items-center gap-.5 text-center sm:items-start sm:text-left mx-auto">
+          <h1 className="max-w-xs text-5xl font-semibold leading-10 tracking-tight text-center dark:text-zinc-50 font-display m-0">
             ECOdometer
           </h1>
-      
+          <p className="text-xl text-center uppercase text-orange">= Running From Empty =</p>
         </div>
-
       </main>
     </div>
   );
