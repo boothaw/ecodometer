aaccd17d9fcc2ce3a0514ad8b3b07a3a8b1ca549
boothaw <andersonwilkesbooth@gmail.com>
2026-01-30 16:32:52 -0500

    profile page via id added

--- src/app/profile/[profileId]/[vehicleId]/page.tsx
+++ src/app/profile/[profileId]/[vehicleId]/page.tsx
@@ -1,19 +1,19 @@
-import { VehicleCard } from "@/src/components/VehicleCard";
+export default async function Vehicles({
+  params,
+}: {
+  params: Promise<{ vehicleId: string }>
+}) {
 
-export default function Profile() {
+  const {vehicle} = await vehicleId
+
   return (
     <div className="flex min-h-screen items-center justify-center font-body">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
-        <div className="flex flex-col items-center justify-center mx-auto gap-6 text-center">
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold text-navy font-display">
-            Profile/Vehicles
+            Vehicle - ID: {vehicle}
           </h1>
-
-          <VehicleCard />
-          <VehicleCard />
-          <VehicleCard />
-          <VehicleCard />
-      
+      <a className="btn" href="/new-vehicle">New Vehicle</a>
         </div>
 
       </main>
---
+++ src/app/profile/[profileId]/page.tsx
@@ -0,0 +1,29 @@
+import { VehicleCard } from "@/src/components/VehicleCard";
+
+export default async function Profile({
+  params,
+}: {
+  params: Promise<{ profileId: string }>
+}) {
+
+    const {profileId} = await params
+
+  return (
+    <div className="flex min-h-screen items-center justify-center font-body">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
+        <div className="flex flex-col items-center justify-center mx-auto gap-6 text-center">
+          <h1 className="max-w-xs text-3xl font-semibold text-navy font-display">
+            Profile/Vehicles Id: {profileId}
+          </h1>
+
+          <VehicleCard />
+          <VehicleCard />
+          <VehicleCard />
+          <VehicleCard />
+      
+        </div>
+
+      </main>
+    </div>
+  );
+}
