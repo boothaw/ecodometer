ebfd41968bfee78dbf8cf8f520f47fe469e75f8f
boothaw <andersonwilkesbooth@gmail.com>
2026-02-11 15:19:39 -0500

    profile page structure

--- src/app/profile/[profileId]/page.tsx
+++ src/app/profile/[profileId]/page.tsx
@@ -1,4 +1,6 @@
 import { VehicleCard } from "@/src/components/VehicleCard";
+import { clerkClient } from '@clerk/nextjs/server';
+
 
 export default async function Profile({
   params,
@@ -8,12 +10,21 @@ export default async function Profile({
 
     const {profileId} = await params
 
+    // console.log(profileId)
+
+    // async function getUserData(userId) {
+    //   const user = await clerkClient.users.getUser(userId);
+    //   console.log(user.username); // Or user.primaryEmailAddress.emailAddress
+    //   return user;
+    // }
+
+
   return (
     <div className="flex min-h-screen items-center justify-center font-body">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
         <div className="flex flex-col items-center justify-center mx-auto gap-6 text-center">
           <h1 className="max-w-xs text-3xl font-semibold text-navy font-display">
-            Profile/Vehicles Id: {profileId}
+            Profile/Vehicles Id:<br></br>{profileId} & 
           </h1>
 
           <VehicleCard />
