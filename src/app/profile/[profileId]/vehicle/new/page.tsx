2f2082ce5aa5fcdc7059d18557ffc505a8be15d6
boothaw <andersonwilkesbooth@gmail.com>
2026-02-13 12:49:49 -0500

    style update

--- src/app/profile/[profileId]/vehicle/new/page.tsx
+++ src/app/profile/[profileId]/vehicle/new/page.tsx
@@ -43,7 +43,7 @@ export default async function NewVehiclePage({
 
   return (
     <div className="flex min-h-screen items-center justify-center font-body">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
+      <main className=" min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
         <div className="card-body card bg-white flex flex-col mx-auto gap-6 text-center sm:text-left w-full max-w-md">
           <h1 className="text-3xl font-semibold text-navy font-display card-title">
             New Vehicle
